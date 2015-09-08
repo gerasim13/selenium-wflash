f9f5e60e1a1f662dfa65cd649bd6b044b6805355
Павел Литвиненко <gerasim13@gmail.com>
2015-09-08 21:52:25 +0300

    Migrate to alpine

---
+++ node/entry_point.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+
+REMOTE_HOST_ADDR=""
+REMOTE_HOST_PORT=""
+REMOTE_HOST_PARAM=""
+
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
+if [ ! -e /opt/selenium/config.json ]; then
+  echo No Selenium Node configuration file, the node-base image is not intended to be run directly. 1>&2
+  exit 1
+fi
+
+if [ ! -z "$HOST_ADDR" ] && [ ! -z "$HOST_PORT" ]; then
+  REMOTE_HOST_ADDR=$HOST_ADDR
+  REMOTE_HOST_PORT=$HOST_PORT
+elif [ ! -z "$HUB_PORT_4444_TCP_ADDR" ]; then
+  REMOTE_HOST_ADDR=$HUB_PORT_4444_TCP_ADDR
+  REMOTE_HOST_PORT=$HUB_PORT_4444_TCP_PORT
+else
+  echo Not linked with a running Hub container 1>&2
+  exit 1
+fi
+
+if [ ! -z "$REMOTE_HOST" ]; then
+  echo "REMOTE_HOST variable is set, appending -remoteHost"
+  REMOTE_HOST_PARAM="-remoteHost $REMOTE_HOST"
+fi
+
+# TODO: Look into http://www.seleniumhq.org/docs/05_selenium_rc.jsp#browser-side-logs
+xvfb-run --server-args="$DISPLAY -screen 0 $GEOMETRY -ac +extension RANDR" \
+  java -jar /opt/selenium/selenium-server-standalone.jar \
+    ${JAVA_OPTS} \
+    -role node \
+    -hub http://$REMOTE_HOST_ADDR:$REMOTE_HOST_PORT/grid/register \
+    ${REMOTE_HOST_PARAM} \
+    -nodeConfig /opt/selenium/config.json &
+NODE_PID=$!
+
+trap shutdown SIGTERM SIGINT
+wait $NODE_PID
